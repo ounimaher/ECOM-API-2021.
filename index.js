4e3bdebbaae3c8968bc94a89415bfc727af82363
maher ouni <ounimaher@gmail.com>
2021-06-26 13:58:28 +0100

    fisrt

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const userController = require('./controllers/userController')
+const app = express()
+app.use(express.json())
+app.use("/user",userController)
+
+app.get('/user', (req, res) => {
+    res.send("Hello API")
+})
+
+app.get('/product/:id', (req, res) => {
+    let productId = req.params.id
+    console.log("productId:" + productId)
+    res.send("Hello  API")
+})
+
+
+
+app.listen(3000, () => console.log("Server started !"))
